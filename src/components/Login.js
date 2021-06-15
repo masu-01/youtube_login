faf04d71b60f789e4bc9a6219df02da390b48aa7
Mayumi Suzuki <l0meet07@gmail.com>
2021-06-16 05:17:10 +0900

    0:35:41　ログイン画面作成とダッシュボードへ遷移

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,30 +3,26 @@ import { Form, Button, Card, Alert } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
 import { Link, useHistory } from "react-router-dom"
 
-export default function Signup() {
+export default function Login() {
     const emailRef = useRef()
     const passwordRef = useRef()
-    const passwordConfirmRef = useRef()
-    const { signup } = useAuth()
+    const { login } = useAuth()
     // const { signup, currentUser } = useAuth()
     const [ error, setError ] = useState("")
     const [ loading, setLoading ] = useState(false)
     const history = useHistory()
 
+
     async function handleSubmit(e) {
         e.preventDefault()
-
-        if(passwordRef.current.value !== passwordConfirmRef.current.value){
-            return setError("パスワードが一致しません")
-        }
 
         try{
             setError("")
             setLoading(true)
-            await signup(emailRef.current.value, passwordRef.current.value)
+            await login(emailRef.current.value, passwordRef.current.value)
             history.push("/")
         }catch{
-            setError("アカウントが作れませんでした")
+            setError("ログインに失敗しました")
         }
 
         setLoading(false)
@@ -38,7 +34,7 @@ export default function Signup() {
         <>
             <Card>
                 <Card.Body>
-                    <h2 className="text-center mb-4">アカウント登録</h2>
+                    <h2 className="text-center mb-4">ログイン</h2>
                     {/* {currentUser.email} */}
                     {error && <Alert variant="danger">{error}</Alert>}
                     <Form onSubmit={handleSubmit}>
@@ -50,16 +46,12 @@ export default function Signup() {
                             <Form.Label>パスワード</Form.Label>
                             <Form.Control type="password" ref={passwordRef} required />
                         </Form.Group>
-                        <Form.Group id="password-confirm">
-                            <Form.Label>パスワード（確認用）</Form.Label>
-                            <Form.Control type="password" ref={passwordConfirmRef} required />
-                        </Form.Group>
-                        <Button disabled={loading} className="w-100 mt-4" type="submit">登録する</Button>
+                        <Button disabled={loading} className="w-100 mt-4" type="submit">ログインする</Button>
                     </Form>
                 </Card.Body>
             </Card>
             <div className="w-100 text-center mt-2">
-                アカウントをお持ちの方は<Link to="/login">こちら</Link>
+                アカウントをお持ちでないかたは<Link to="/signup">こちら</Link>
             </div>
             
         </>
